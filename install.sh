6292cbc34e88878239b6b0248ad899adb6b3f21f
Daniel Majewski <meroz@protonmail.com>
2020-03-30 21:20:11 +0200

    get reponse from file

---
+++ install.sh
@@ -0,0 +1,2 @@
+python3.6 setup.py sdist
+pip3 install dist/microapi-0.1.tar.gz
--- microapi/Server.py
+++ microapi/Server.py
@@ -1,3 +1,5 @@
+from json import loads, dumps
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from microapi.args_parser import args_parser
 
@@ -9,18 +11,34 @@ class Server(BaseHTTPRequestHandler):
         self.response_code = args.c
         self.data = args.d
         self.api_path = args.e if hasattr(args, 'e') else ''
+        self.response_file_path = args.f
 
         super().__init__(request, client_address, server)
+
+    def get_response_data_from_file(self):
+        if not self.response_file_path:
+            return None
+
+        with open(self.response_file_path, 'r') as file:
+            return loads(file.read())
 
     def response(self):
         self.send_response(self.response_code)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
 
+        file_data = self.get_response_data_from_file()
+
         if self.path == f'/{self.api_path}':
-            self.send(self.data)
+            if file_data:
+                self.send(dumps(file_data))
+            else:
+                self.send(self.data)
         else:
             self.send('No endpoint')
+
+    def send(self, data):
+        self.wfile.write(data.encode('utf-8'))
 
     def do_GET(self):
         self.response()
@@ -39,9 +57,6 @@ class Server(BaseHTTPRequestHandler):
 
     def do_HEAD(self):
         self.response()
-
-    def send(self, data):
-        self.wfile.write(data.encode('utf-8'))
 
 
 def run_server(port):
